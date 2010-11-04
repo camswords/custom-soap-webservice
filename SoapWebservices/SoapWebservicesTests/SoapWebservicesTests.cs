69b95286e93865258c176a577071c39d9e4360ca
Cam <cam>
2010-11-04 21:04:23 +0000

    http gateway test passes

--- SoapWebservices/SoapWebservicesTests/SoapWebservicesTests.cs
+++ SoapWebservices/SoapWebservicesTests/SoapWebservicesTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Diagnostics;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
+using NUnit.Framework.SyntaxHelpers;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
@@ -33,18 +34,13 @@ namespace SoapWebservicesTests
            
             public void Begin()
             {
-                Printer.Print("begin accept request");
                 var tcpClient = serverSocket.AcceptTcpClient();
 
-                Printer.Print("creating sockets");
                 var stream = tcpClient.GetStream();
                 var reader = new StreamReader(stream);
                 var writer = new StreamWriter(stream);
 
                 var request = "";
-                var response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
-
-                Printer.Print("reading");
 
                 string line = null;
                 do
@@ -53,56 +49,39 @@ namespace SoapWebservicesTests
                     request += line + "\n";
                 } while (line.Length != 0);
 
-                Printer.Print("read request: " + request);
-                
 
-                Printer.Print("writing");
-
-                writer.Write(response);
+                writer.WriteLine("HTTP/1.1 200 OK");
+                writer.WriteLine("");
+                writer.Write("yeah");
                 writer.Flush();
 
                 stream.Dispose();
-        
-                Printer.Print("done sockets!");
             }
         }
 
         [Test]
         public void should_return_response()
         {
-
-            Printer.Print("initialising server");
             var svr = new Server();
    
             Thread thread = new Thread(svr.Begin);
             thread.Start();
 
-
-            Printer.Print("server started, calling get");
-
             var request = WebRequest.Create("http://localhost:5021");
-            Printer.Print("getting response");
-            
-            var response = request.GetResponse();
+            var response = (HttpWebResponse) request.GetResponse();
 
             var contents = "test.no.contents.returned";
-            Printer.Print("reading response");
 
             using(var s = new StreamReader(response.GetResponseStream()))
             {
-                Printer.Print("reading to end");
                 contents = s.ReadToEnd();
             }
             
-            Printer.Print("finished, lets join forces");
-
             thread.Join(10000);
 
-            Printer.Print("read contents: " + contents);
-
-            Printer.Print("ok, finished so joining threads");
-
-            Assert.AreEqual("yeah", "yeah");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.StatusDescription, Is.EqualTo("OK"));
+            Assert.That(contents, Is.EqualTo("yeah"));
         }
     }
 
